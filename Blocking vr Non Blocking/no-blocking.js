76d3766bd83e8d067977afbf88ea23076846b681
jhormancely <jhormancely@gmail.com>
2022-07-17 05:07:16 -0500

    modificacion noblocking

--- Blocking vr Non Blocking/no-blocking.js
+++ Blocking vr Non Blocking/no-blocking.js
@@ -2,15 +2,15 @@ const { obtenerUsuarioAsyn } = require("./usuario");
 console.log("Inicio de programa");
 console.time("Inicio");
 
-obtenerUsuarioAsyn(1, mostrar_usuario);
-
 mostrar_usuario = (usuario) => {
   console.log("Usuario1: ", usuario);
 };
+
+obtenerUsuarioAsyn(1, mostrar_usuario);
 
 obtenerUsuarioAsyn(2, (usuario) => {
   console.log("Usuario2: ", usuario);
 });
 
 console.log("Fin del programa");
-console.Endtime("Inicio");
+console.timeEnd("Inicio");
